bbfd71f25a50ec7b35485cbfb246018beeeae901
Arsenic110 <frameshiftdrive110@gmail.com>
2022-03-15 18:51:49 +0000

    gpio v6

--- server/server.js
+++ server/server.js
@@ -93,7 +93,7 @@ function sockets(socket)
     {
         console.log("SocketIO: Writing to LED");
         LED.digitalWrite(1);
-        sleep(config.hardware.LEDTimeout);
+        await sleep(config.hardware.LEDTimeout);
         LED.digitalWrite(0);
     });
 }
