4fd98d4debfd25193cb2031e0e4146b77d110e13
Arsenic110 <frameshiftdrive110@gmail.com>
2022-03-16 12:46:17 +0000

    Added slider & fixed Jquery

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,18 @@ var socket = io();
 
 socket.emit("init", "Hewoooo!!");
 
+$('#slider').on("change", () =>
+{
+    $('#thermo-value')[0].innerHTML = "Value: " +  $("#slider").val();
+    socket.emit('thermostat', $("#slider").val(), 5);
+
+});
+
+console.log($('#thermo-value')[0]);
+
+//window.setInterval(update, 20);
+
+function update()
+{
+    $('#thermo-value')[0].innerHTML = $("#slider").val();
+}
